c84e7972249d72b65cab2b76f52054d16785ea29
Restkand <reiskand07@gmail.com>
2023-03-02 17:53:52 +0700

    Menambahkan Pages Ketiga, Coffee Creation

--- myscript.js
+++ myscript.js
@@ -1,6 +1,14 @@
 var currentSlide = 0;
+var currentSlideCoffe = 0;
+var slideIndex = 0;
+// SlideShow PagesSecondImage
 var slides = document.querySelectorAll("#slideshow img");
 var slideCount = slides.length;
+// SlideShow PagesThirdImage
+var slidesCoffee = document.querySelectorAll("#slideshowCoffee img");
+var slideCountCoffee = slidesCoffee.length;
+
+
 
 function gantiSlideKinari() {
     slides[currentSlide].style.opacity = 0;
@@ -8,4 +16,12 @@ function gantiSlideKinari() {
     slides[currentSlide].style.opacity = 1;
 }
 
-setInterval(gantiSlideKinari, 5500); // Waktu delay dalam milidetik (3 detik dalam contoh ini)
+function gantiSlideCoffee() {
+    slidesCoffee[currentSlideCoffe].style.opacity = 0;
+    currentSlideCoffe = (currentSlideCoffe + 1) % slideCountCoffee;
+    slidesCoffee[currentSlideCoffe].style.opacity = 1;
+}
+
+
+setInterval(gantiSlideKinari, 5500);
+setInterval(gantiSlideCoffee, 5500);
